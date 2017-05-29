8243368557711ec716cb309698a82a60b8513a5e
Deepak Abraham Tom <deepaktabraham@gmail.com>
2017-05-29 15:58:40 -0600

    Updating IOCTL

--- Simple Character Driver/mycdev.h
+++ Simple Character Driver/mycdev.h
@@ -1,8 +1,8 @@
-#ifndef H_ASP_CLEAR_BUF
-#define H_ASP_CLEAR_BUF
+#ifndef H_CLEAR_BUF
+#define H_CLEAR_BUF
 
 #include <linux/ioctl.h>
 
-#define ASP_CLEAR_BUF _IO('k', 1)
+#define CLEAR_BUF _IO('k', 1)
 
 #endif
